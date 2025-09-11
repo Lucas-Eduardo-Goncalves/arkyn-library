56c759436fc28333a8d5ed23eccfd33847d1c0f9
Lucas Gonçalves <lucasgoncalvesgithub@gmail.com>
2025-09-11 11:51:24 -0300

    chore(components, server, shared, templates): update package versions and add Vite configuration
    - Updated package versions from 3.0.1-beta.58 to 3.0.1-beta.59 across all
      components, server, shared, and templates.
    - Added Vite configuration files for server, shared, and templates packages
      to facilitate modern build processes.
    - Adjusted npmignore files to include Vite configuration and other
      development dependencies.
    - Modified TypeScript configuration to allow synthetic default imports
      and included Vite config in the exclude list.
    
    This update enhances the build process and ensures all packages are
    using the latest version.

---
+++ packages/templates/vite.config.ts
@@ -0,0 +1,19 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [],
+  build: {
+    minify: true,
+    outDir: "./dist",
+    rollupOptions: {
+      external: [],
+      output: { format: "esm" },
+    },
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "@arkyn/templates",
+      fileName: "bundle",
+    },
+  },
+});
