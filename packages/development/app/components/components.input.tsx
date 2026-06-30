d0d52fc0d4855703e628d3f35b3e555a9fdca942
Lucas Gonçalves <lucasgoncalves@arkyn.dev>
2026-06-30 11:16:11 -0300

    chore(components, server, shared, templates): update version to 3.0.1-beta.179
    - Updated version number in package.json files for:
      - @arkyn/components
      - @arkyn/server
      - @arkyn/shared
      - @arkyn/templates
    - Ensured consistency across all packages with the new versioning
    - No functional changes were made to the codebase

--- packages/development/app/components/components.input.tsx
+++ packages/development/app/components/components.input.tsx
@@ -1,4 +1,5 @@
 import { Input } from "@arkyn/components/input";
+import { Circle } from "lucide-react";
 
 export default function InputRoute() {
 	return (
@@ -21,6 +22,37 @@ export default function InputRoute() {
 					variant="underline"
 					label="Underline:"
 					placeholder="Digite algo..."
+				/>
+			</div>
+
+			<div className="exampleContainer row foreground">
+				<Input
+					name="test"
+					variant="outline"
+					label="Outline:"
+					placeholder="Digite algo..."
+					prefix="$"
+				/>
+				<Input
+					name="test"
+					variant="solid"
+					label="Solid:"
+					placeholder="Digite algo..."
+					suffix="@"
+				/>
+				<Input
+					name="test"
+					variant="outline"
+					label="Outline:"
+					placeholder="Digite algo..."
+					leftIcon={Circle}
+				/>
+				<Input
+					name="test"
+					variant="solid"
+					label="Solid:"
+					placeholder="Digite algo..."
+					rightIcon={Circle}
 				/>
 			</div>
 
