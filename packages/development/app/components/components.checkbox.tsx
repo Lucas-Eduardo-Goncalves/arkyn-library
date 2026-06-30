8366983064bcf97744dd42f441bc5ff4a48fdee6
Lucas Gonçalves <lucasgoncalves@arkyn.dev>
2026-06-30 10:05:18 -0300

    feat(components): enhance input and checkbox components with labels
    - Updated the Checkbox component to include a label for better
      accessibility and clarity.
    - Modified the Input component to add labels and placeholders for
      each variant (outline, solid, underline) to improve user experience.
    - Adjusted the CSS variable for input background color in dark mode
      for better contrast and visibility.

--- packages/development/app/components/components.checkbox.tsx
+++ packages/development/app/components/components.checkbox.tsx
@@ -2,8 +2,14 @@ import { Checkbox } from "@arkyn/components/checkbox";
 
 export default function CheckboxRoute() {
 	return (
-		<div className="exampleContainer row">
-			<Checkbox name="test" />
-		</div>
+		<>
+			<div className="exampleContainer row foreground">
+				<Checkbox name="test" label="Teste" />
+			</div>
+
+			<div className="exampleContainer row">
+				<Checkbox name="test" label="Teste" />
+			</div>
+		</>
 	);
 }
--- packages/development/app/components/components.input.tsx
+++ packages/development/app/components/components.input.tsx
@@ -4,15 +4,45 @@ export default function InputRoute() {
 	return (
 		<>
 			<div className="exampleContainer row foreground">
-				<Input name="test" variant="outline" />
-				<Input name="test" variant="solid" />
-				<Input name="test" variant="underline" />
+				<Input
+					name="test"
+					variant="outline"
+					label="Outline:"
+					placeholder="Digite algo..."
+				/>
+				<Input
+					name="test"
+					variant="solid"
+					label="Solid:"
+					placeholder="Digite algo..."
+				/>
+				<Input
+					name="test"
+					variant="underline"
+					label="Underline:"
+					placeholder="Digite algo..."
+				/>
 			</div>
 
 			<div className="exampleContainer row">
-				<Input name="test" variant="outline" />
-				<Input name="test" variant="solid" />
-				<Input name="test" variant="underline" />
+				<Input
+					name="test"
+					variant="outline"
+					label="Outline:"
+					placeholder="Digite algo..."
+				/>
+				<Input
+					name="test"
+					variant="solid"
+					label="Solid:"
+					placeholder="Digite algo..."
+				/>
+				<Input
+					name="test"
+					variant="underline"
+					label="Underline:"
+					placeholder="Digite algo..."
+				/>
 			</div>
 		</>
 	);
